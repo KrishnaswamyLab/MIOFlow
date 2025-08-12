02f35e716bd9bb89e68d1851c88c309f31ca5020
Joao Felipe Rocha <joaofelipe.rocha@.yale.edu>
2025-08-12 15:10:06 -0300

    Added uv to .toml

---
+++ main.py
@@ -0,0 +1,10 @@
+import MIOFlow
+
+def main():
+    print("Hello from mioflow!")
+    print(MIOFlow.__version__)
+    # print(MIOFlow.models)
+    # print(MIOFlow.mioflow)
+
+if __name__ == "__main__":
+    main()
